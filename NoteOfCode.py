d052f4d72aa9ad2b1654bcc77019aa0a270ea1f4
York <59372141+Dermaed@users.noreply.github.com>
2020-01-17 22:05:46 +0300

    ///

--- NoteOfCode.py
+++ NoteOfCode.py
@@ -52,21 +52,37 @@ head_left = pygame.image.load('pictures/Head/Head-left.png').convert()
 head_up = pygame.image.load('pictures/Head/Head-up.png').convert()
 head_down = pygame.image.load('pictures/Head/Head-down.png').convert()
 
-body1 = pygame.image.load('Body/Body1.png').convert()
-body2 = pygame.image.load('Body/Body2.png').convert()
-body3 = pygame.image.load('Body/Body3.png').convert()
-body4 = pygame.image.load('Body/Body4.png').convert()
-body5 = pygame.image.load('Body/Body5.png').convert()
-body6 = pygame.image.load('Body/Body6.png').convert()
-body7 = pygame.image.load('Body/Body7.png').convert()
-body8 = pygame.image.load('Body/Body8.png').convert()
-body9 = pygame.image.load('Body/Body9.png').convert()
-body10 = pygame.image.load('Body/Body10.png').convert()
-body11 = pygame.image.load('Body/Body11.png').convert()
-body12 = pygame.image.load('Body/Body12.png').convert()
-body13 = pygame.image.load('Body/Body13.png').convert()
-body14 = pygame.image.load('Body/Body14.png').convert()
+body1 = pygame.image.load('pictures/Body/Body1.png').convert()
+body2 = pygame.image.load('pictures/Body/Body2.png').convert()
+body3 = pygame.image.load('pictures/Body/Body3.png').convert()
+body4 = pygame.image.load('pictures/Body/Body4.png').convert()
+body5 = pygame.image.load('pictures/Body/Body5.png').convert()
+body6 = pygame.image.load('pictures/Body/Body6.png').convert()
+body7 = pygame.image.load('pictures/Body/Body7.png').convert()
+body8 = pygame.image.load('pictures/Body/Body8.png').convert()
+body9 = pygame.image.load('pictures/Body/Body9.png').convert()
+body10 = pygame.image.load('pictures/Body/Body10.png').convert()
+body11 = pygame.image.load('pictures/Body/Body11.png').convert()
+body12 = pygame.image.load('pictures/Body/Body12.png').convert()
+body13 = pygame.image.load('pictures/Body/Body13.png').convert()
+body14 = pygame.image.load('pictures/Body/Body14.png').convert()
 
+i = random.randrange(0,14)
+#if i == 1:
+    #screen.blit(len(body[]))
+#if i == 2:
+#if i == 3:
+#if i == 4:
+#if i == 5:
+#if i == 6:
+#if i == 7:
+#if i == 8:
+#if i == 9:
+#if i == 10:
+#if i == 11:
+#if i == 12:
+#if i == 13:
+#if i == 14:
 
 # Creating Food
 food0 = (random.randrange(0,20)*20)+20
@@ -165,6 +181,8 @@ while not done:
         food1 = (random.randrange(0,20)*20)+20
         body.append(body[0])
         xNS += 1
+        j = len(body)
+        print(j)
         # Score line
     pygame.draw.rect(screen, BLUE, (20,425,xNS,15))
     # if xNS get 100 points write "You win!" and exit the game
