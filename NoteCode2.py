cee5b8098d17965156fd1bc9b63a602cd60643fe
York <59372141+Dermaed@users.noreply.github.com>
2020-01-18 01:05:15 +0300

    Сlasses
    shame on me. I'm trying to translate code into classes for a better understanding

--- NoteCode2.py
+++ NoteCode2.py
@@ -1,8 +1,85 @@
 body = [[82,82],[62,82],[42,82]]
-x = len(body[0])
+x = len(body)
 g = body[[0][0]]
 g1 = len(body[[0][0]])
-
+g2 = body[[1][0]]
 print(x)
 print(g)
 print(g1)
+print(g2)
+#if i == 1:
+    #g = len(body)
+
+    if i == 1:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body1,(k[0],k[1]))
+    elif i == 2:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body2,(k[0],k[1]))
+    elif i == 3:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body3,(k[0],k[1]))
+    elif i == 4:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body4,(k[0],k[1]))
+    elif i == 5:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body5,(k[0],k[1]))
+    elif i == 6:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body6,(k[0],k[1]))
+    elif i == 7:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body7,(k[0],k[1]))
+    elif i == 8:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body8,(k[0],k[1]))
+    elif i == 9:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body9,(k[0],k[1]))
+    elif i == 10:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body10,(k[0],k[1]))
+    elif i == 11:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body11,(k[0],k[1]))
+    elif i == 12:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body12,(k[0],k[1]))
+    elif i == 13:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body13,(k[0],k[1]))
+    elif i == 14:
+        g = len(body)
+        k = [[g][0]]
+        screen.blit(body14,(k[0],k[1]))
+
+        body1 = pygame.image.load('pictures/Body/Body1.png').convert()
+        body2 = pygame.image.load('pictures/Body/Body2.png').convert()
+        body3 = pygame.image.load('pictures/Body/Body3.png').convert()
+        body4 = pygame.image.load('pictures/Body/Body4.png').convert()
+        body5 = pygame.image.load('pictures/Body/Body5.png').convert()
+        body6 = pygame.image.load('pictures/Body/Body6.png').convert()
+        body7 = pygame.image.load('pictures/Body/Body7.png').convert()
+        body8 = pygame.image.load('pictures/Body/Body8.png').convert()
+        body9 = pygame.image.load('pictures/Body/Body9.png').convert()
+        body10 = pygame.image.load('pictures/Body/Body10.png').convert()
+        body11 = pygame.image.load('pictures/Body/Body11.png').convert()
+        body12 = pygame.image.load('pictures/Body/Body12.png').convert()
+        body13 = pygame.image.load('pictures/Body/Body13.png').convert()
+        body14 = pygame.image.load('pictures/Body/Body14.png').convert()
+
+        i = random.randrange(0,14)
--- NoteOfCode.py
+++ NoteOfCode.py
@@ -26,7 +26,29 @@ xNS = 1
 
 # Creating Snake
 # Head
-snake = [82,82]
+class Snake():
+    def __init__(self, image_pth):
+        self.coordinates = [82,82]
+        self.direction =  "Right"
+        self.image_pth = pygame.image.load(image_pth).convert()
+    def move():
+        if event.type == pygame.KEYDOWN:
+            elif event.key == pygame.K_RIGHT and goes_side != "Left":
+                goes_side = "Right"
+            elif event.key == pygame.K_LEFT and goes_side != "Right":
+                goes_side = "Left"
+            elif event.key == pygame.K_UP and goes_side != "Down":
+                goes_side = "Up"
+            elif event.key == pygame.K_DOWN and goes_side != "Up":
+                goes_side = "Down"
+    def hit_body():
+        for hit in body:
+            if hit[0] == head[0] and hit[1] == head[1]:
+                print("You lose!")
+                sys.exit()
+
+
+head = [82,82]
 # Start direction
 goes_side = "Right"
 # body
@@ -34,7 +56,7 @@ body = [[82,82],[62,82],[42,82]]
 segment = []
 # Creating logic Body of snake
 def animation():
-    body.insert(0,list(snake))
+    body.insert(0,list(head))
     body.pop()
 # DELETED this code i wrote in Dreawing code DELETED
     #def draw_snake():
@@ -52,38 +74,6 @@ head_left = pygame.image.load('pictures/Head/Head-left.png').convert()
 head_up = pygame.image.load('pictures/Head/Head-up.png').convert()
 head_down = pygame.image.load('pictures/Head/Head-down.png').convert()
 
-body1 = pygame.image.load('pictures/Body/Body1.png').convert()
-body2 = pygame.image.load('pictures/Body/Body2.png').convert()
-body3 = pygame.image.load('pictures/Body/Body3.png').convert()
-body4 = pygame.image.load('pictures/Body/Body4.png').convert()
-body5 = pygame.image.load('pictures/Body/Body5.png').convert()
-body6 = pygame.image.load('pictures/Body/Body6.png').convert()
-body7 = pygame.image.load('pictures/Body/Body7.png').convert()
-body8 = pygame.image.load('pictures/Body/Body8.png').convert()
-body9 = pygame.image.load('pictures/Body/Body9.png').convert()
-body10 = pygame.image.load('pictures/Body/Body10.png').convert()
-body11 = pygame.image.load('pictures/Body/Body11.png').convert()
-body12 = pygame.image.load('pictures/Body/Body12.png').convert()
-body13 = pygame.image.load('pictures/Body/Body13.png').convert()
-body14 = pygame.image.load('pictures/Body/Body14.png').convert()
-
-i = random.randrange(0,14)
-#if i == 1:
-    #screen.blit(len(body[]))
-#if i == 2:
-#if i == 3:
-#if i == 4:
-#if i == 5:
-#if i == 6:
-#if i == 7:
-#if i == 8:
-#if i == 9:
-#if i == 10:
-#if i == 11:
-#if i == 12:
-#if i == 13:
-#if i == 14:
-
 # Creating Food
 food0 = (random.randrange(0,20)*20)+20
 food1 = (random.randrange(0,20)*20)+20
@@ -94,44 +84,33 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
             # Control of snake
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT and goes_side != "Left":
-                goes_side = "Right"
-            elif event.key == pygame.K_LEFT and goes_side != "Right":
-                goes_side = "Left"
-            elif event.key == pygame.K_UP and goes_side != "Down":
-                goes_side = "Up"
-            elif event.key == pygame.K_DOWN and goes_side != "Up":
-                goes_side = "Down"
+
             if event.key == pygame.K_ESCAPE:
                 sys.exit()
     # --- Game logic should go here
     # Where snake looks, there snake goes
     if goes_side == "Right":
-        snake[0] += 20
+        head[0] += 20
     if goes_side == "Left":
-        snake[0] -= 20
+        head[0] -= 20
     if goes_side == "Up":
-        snake[1] -= 20
+        head[1] -= 20
     if goes_side == "Down":
-        snake[1] += 20
+        head[1] += 20
     # Here i created cell for snake
-    if snake[0] > 402:
-        snake[0] -= 20
-    if snake[0] < 22:
-        snake[0] += 20
-    if snake[1] > 402:
-        snake[1] -= 20
-    if snake[1] < 22:
-        snake[1] += 20
+    if head[0] > 402:
+        head[0] -= 20
+    if head[0] < 22:
+        head[0] += 20
+    if head[1] > 402:
+        head[1] -= 20
+    if head[1] < 22:
+        head[1] += 20
 
     # ---- logic of Clash head and body
-    for k in body:
-        if k[0] == snake[0] and k[1] == snake[1]:
-            print("You lose!")
-            sys.exit()
-    for l in body:
-        if l[0] == food0+2 and l[1] == food1+2:
+    snake.hit_body()
+    for food_spawn in body:
+        if food_spawn[0] == food0+2 and food_spawn[1] == food1+2:
             food0 = (random.randrange(0,20)*20)+20
             food1 = (random.randrange(0,20)*20)+20
     # --- Screen-clearing code goes here
@@ -165,14 +144,14 @@ while not done:
     animation()
     # Drawing Head of snake
 
-    pygame.draw.rect(screen, GREEN, (snake[0], snake[1], 16, 16))
+    pygame.draw.rect(screen, GREEN, (head[0], head[1], 16, 16))
     # Drawing body of snake
     for segment in body:
         pygame.draw.rect(screen, GREEN, pygame.Rect(segment[0], segment[1],16,16))
     # Drawing food
     pygame.draw.rect(screen, RED, (food0+2, food1+2, 16, 16))
     # Condition of eating food
-    if food0 < snake[0] < food0+20 and food1 < snake[1] < food1+20:
+    if food0 < head[0] < food0+20 and food1 < head[1] < food1+20:
         # Score + 1
         NewScore += 1
         print(NewScore)
@@ -181,8 +160,7 @@ while not done:
         food1 = (random.randrange(0,20)*20)+20
         body.append(body[0])
         xNS += 1
-        j = len(body)
-        print(j)
+        i = random.randrange(0,14)
         # Score line
     pygame.draw.rect(screen, BLUE, (20,425,xNS,15))
     # if xNS get 100 points write "You win!" and exit the game
@@ -191,13 +169,14 @@ while not done:
         sys.exit()
 
     if goes_side == "Right":
-        screen.blit(head_right, (snake[0], snake[1]))
+        screen.blit(head_right, (head[0], head[1]))
     if goes_side == "Left":
-        screen.blit(head_left, (snake[0], snake[1]))
+        screen.blit(head_left, (head[0], head[1]))
     if goes_side == "Up":
-        screen.blit(head_up, (snake[0], snake[1]))
+        screen.blit(head_up, (head[0], head[1]))
     if goes_side == "Down":
-        screen.blit(head_down, (snake[0], snake[1]))
+        screen.blit(head_down, (head[0], head[1]))
+
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
