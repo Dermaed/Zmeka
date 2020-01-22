0824228d8f11fff5298ba1813ce78e8870417648
York <59372141+Dermaed@users.noreply.github.com>
2020-01-23 00:03:19 +0300

    10 hours working
    In this time i made: Pause before game will start and integrated into the code, everytime when player die time stop, and if press R game will restart. And last i tryed add some guide how to play.

--- Zmeka.py
+++ Zmeka.py
@@ -1,11 +1,11 @@
 # import all libraries which are uses
 import pygame
+import random
 import sys
 import math
-import random
 
 # Define some colors
-BLACK = (0, 0, 0)
+BLACK = (20, 60, 20)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 YELLOW = (255, 255, 0)
@@ -19,69 +19,110 @@ pygame.init()
 # Set the width and height of the screen [width, height]
 size = (440, 445)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("ZMEKA")
 
-# --------- Создание гл. Меню ---------
+
+#class GuideSettings():
+#    def __init__(self, x, y, filename):
+#        self.x = x
+#        self.y = y
+#        self.bitmap = pygame.image.load(filename).convert
+#
+#    def page(self):
+#        done = True
+#        while done:
+#            for event in pygame.event.get():
+#                if event.type == pygame.QUIT:
+#                    done = False
+#                elif event.type == pygame.KEYDOWN:
+#                    if event.key == pygame.K_ESCAPE:
+#                        sys.exit()
+#        screen.blit(self.bitmap, (self.x, self.y))
+#        pygame.display.flip()
+#
+#
+#guide = GuideSettings(0, 0, "pictures/Main Menu/Control menu.png")
+#guide.page()
+
+
+# --------- Creating main menu ---------
 class Menu():
     def __init__(self, x, y, filename):
         self.x = x
         self.y = y
         self.bitmap = pygame.image.load(filename)
+
     def menu(self):
         done = True
-        # -- Позиция кнопки Start --
-        # Верхний левый угл Start
-        start_pos = (120,170)
+        # -- Pos button Start --
+        # Left-up angle of Start
+        start_pos = (120, 170)
         s_p_x = start_pos[0]
         s_p_y = start_pos[1]
-        # Правый нижний угл Start
-        start_pos1 = (320,210)
+        # Right-down angle of Start
+        start_pos1 = (320, 210)
         s_p1_x = start_pos1[0]
         s_p1_y = start_pos1[1]
-        # -- Позиция кнопки Quit --
-        quit_pos = (120, 250)
-        # Верхний левый угл Quit
+        # -- Pos button Quit --
+        quit_pos = (120, 330)
+        # Left-up angle of Quit
         q_p_x = quit_pos[0]
         q_p_y = quit_pos[1]
-        quit_pos1 = (320, 291)
-        # Правый нижний угл Quit
+        quit_pos1 = (320, 371)
+        # Right-down angle of Quit
         q_p1_x = quit_pos1[0]
         q_p1_y = quit_pos1[1]
-        # ---- Гл. Цикл гл. меню ----
+        # -- pos button of Control --
+        control = (120, 250)
+        # Left-up angle of Control
+        c_p_x = control[0]
+        c_p_y = control[1]
+        control1 = (320, 291)
+        # Right-down angle of Control
+        c_p1_x = control1[0]
+        c_p1_y = control1[1]
+        # ---- Main loop for main menu ----
         while done:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     done = False
-                    # Работа с мышкой
+                    # Checer of mouse clicker
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    # Опознание позиции мышки
+                    # Detector of mouse pos
                     pos = pygame.mouse.get_pos()
                     posx = pos[0]
                     posy = pos[1]
-                    # Проверка позиции мышки и кнопки Start
+                    # Check pos of mouse and button Start
                     if s_p_x < posx < s_p1_x and s_p_y < posy < s_p1_y:
                         done = False
+                    elif c_p_x < posx < c_p1_x and c_p_y < posy < c_p1_y:
+                        pass
                     elif q_p_x < posx < q_p1_x and q_p_y < posy < q_p1_y:
                         sys.exit()
-                    # Выход из игры через кнопку ESCAPE
+                    # Exit from the game when you press ESCAPE
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         sys.exit()
-
+                    # Start game when you press SPACE
+                    elif event.key == pygame.K_SPACE:
+                        done = False
             screen.blit(self.bitmap, (self.x, self.y))
             pygame.display.flip()
-# Даём координаты и запускаем выше написаный код
-plunt = Menu(0,0,"pictures/Main Menu/main_menu.png" )
-# Применение цикла в классе
+
+
+# Creating main menu
+plunt = Menu(0, 0, "pictures/Main Menu/main_menu.png")
 plunt.menu()
+
+
 # Creating Snake
 class Snake():
     # Adding pictures for changing picture by direction
     def __init__(self, image_pth):
         # x, y position head of snake
-        self.head = [82, 82]
+        self.head = [122, 202]
         # mass of x and y position of all body of snake
-        self.body = [[82, 82], [62, 82], [42, 82]]
+        self.body = [[122, 202], [102, 202], [82, 202]]
         # direction where snake goes
         self.direction = "Right"
         # sprite of snake
@@ -99,6 +140,10 @@ class Snake():
         self.line_to_win = 1
         # detector for food, it won't spawn in snake
         self.detector = []
+        # stoping time for few checks
+        self.time_stop = True
+        # variable for continuation or exit the game
+        self.retry = ""
 
     def draw_head(self):
         pygame.draw.rect(screen, YELLOW, (self.head[0], self.head[1], 16, 16))
@@ -119,13 +164,27 @@ class Snake():
                     self.direction = "Down"
                 elif event.key == pygame.K_ESCAPE:
                     sys.exit()
+                # when you press SPACE game will start
+                elif event.key == pygame.K_SPACE:
+                    self.time_stop = False
+                elif event.key == pygame.K_r:
+                    self.head = [122, 202]
+                    self.body = [[122, 202], [102, 202], [82, 202]]
+                    self.direction = "Right"
+                    self.Score = 0
+                    self.food_x = (random.randrange(0, 20) * 20) + 20
+                    self.food_y = (random.randrange(0, 20) * 20) + 20
+                    self.retry = ""
+                    self.line_to_win = 1
 
     # detecting head and body crush
     def hit_body(self):
-        for hit in self.body:
-            if hit[0] == self.head[0] and hit[1] == self.head[1]:
-                print("You lose!")
-                sys.exit()
+        if self.time_stop == False:
+            for hit in self.body:
+                if hit[0] == self.head[0] and hit[1] == self.head[1]:
+                    print("You lose!")
+                    # Stoping time for check
+                    self.time_stop = True
 
     # detecting that food won't spawn in body
     def exclusion_stucking_food(self):
@@ -136,14 +195,15 @@ class Snake():
 
     # That is logic how snake goes on a field
     def logic_move(self):
-        if self.direction == "Right":
-            self.head[0] += 20
-        if self.direction == "Left":
-            self.head[0] -= 20
-        if self.direction == "Up":
-            self.head[1] -= 20
-        if self.direction == "Down":
-            self.head[1] += 20
+        if self.time_stop == False:
+            if self.direction == "Right":
+                self.head[0] += 20
+            if self.direction == "Left":
+                self.head[0] -= 20
+            if self.direction == "Up":
+                self.head[1] -= 20
+            if self.direction == "Down":
+                self.head[1] += 20
 
     # That is logic where snake can't go
     def field_snake(self):
@@ -184,8 +244,9 @@ class Snake():
 
     # Logic that snake follow the head
     def animation(self):
-        self.body.insert(0, list(self.head))
-        self.body.pop()
+        if self.time_stop == False:
+            self.body.insert(0, list(self.head))
+            self.body.pop()
 
     def draw_body(self):
         self.segment = []
@@ -193,6 +254,7 @@ class Snake():
             pygame.draw.rect(screen, YELLOW, pygame.Rect(segment[0], segment[1], 16, 16))
 
 
+# Creating snake
 snake = Snake("pictures/Head/Head-right.png")
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
@@ -240,16 +302,16 @@ while not snake.done:
     # Drawing food
     snake.drawing_food()
     snake.eating_food()
+
     # "LINE TO WIN"
     pygame.draw.rect(screen, BLUE, (20, 425, snake.line_to_win, 15))
     # That is check when score goes 100+ points
     if snake.line_to_win > 100:
         print("You win!")
         sys.exit()
+
     snake.sprites()
 
     pygame.display.flip()
-
     clock.tick(10)
-
 pygame.quit()
